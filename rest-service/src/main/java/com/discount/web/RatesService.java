cd864c303a39b02c5f2b63f6beffa6653f63965b
tal <talpor30@gmail.com>
2020-02-09 13:16:32 +0200

    fuse - discount rest service

---
+++ rest-service/src/main/java/com/discount/web/RatesService.java
@@ -0,0 +1,39 @@
+/*
+ * #%L
+ * Wildfly Camel :: Example :: Camel CXF JAX-RS
+ * %%
+ * Copyright (C) 2013 - 2016 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.discount.web;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/exchange")
+public interface RatesService {
+
+	@POST
+	@Path("/sendXml")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response sendXml(String xmlData);
+
+}
+
